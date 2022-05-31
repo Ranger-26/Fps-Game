eeb9e80611979f208e7ca8217532bdebc3ac6691
smohan67 <siddhu.mohan6@gmail.com>
2022-05-31 00:15:22 -0500

    t

---
+++ Assets/Scripts/GunData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun", menuName = "Item")]
+public class GunData : ScriptableObject
+{
+    public AudioClip[] GunShootSounds;
+
+    public AudioClip[] GunHitSounds;
+
+    public float Range;
+
+    public FiringMode FiringMode;
+
+    public float FireRate;
+}
---
+++ Assets/Scripts/GunShoot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GunShoot : MonoBehaviour
+{
+    public GunData GunData;
+
+    public UnityEvent OnGunShot;
+
+    public UnityEvent OnGunDraw;
+
+    public FirePoint FirePoint;
+
+    private void Start()
+    {
+        if (FirePoint == null)
+        {
+            FirePoint = GetComponentInChildren<FirePoint>();
+        }
+    }
+
+    private void Update()
+    {
+        if (GunData.FiringMode == FiringMode.SemiAuto)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                InvokeRepeating(nameof(Shoot), 0,  GunData.FireRate);
+            }
+            else if (Input.GetButtonUp("Fire1"))
+            {
+                CancelInvoke(nameof(Shoot));
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        
+    }
+}
