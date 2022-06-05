bf9d47561ca8b4e897f7ea6309f9c012f418d009
smohan67 <siddhu.mohan6@gmail.com>
2022-06-05 17:21:26 -0500

    yes

---
+++ Assets/Scripts/GunShoot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShoot : MonoBehaviour
+{
+    public GunData GunData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
